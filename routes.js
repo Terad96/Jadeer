aed5d251165f0b60104685ce756d7c3cb14aa2f2
Terad96 <Terad.Alturkistani@gmail.com>
2023-02-17 21:45:53 +0300

    Initial commit

---
+++ routes.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req,res){
+
+    console.log("I'm on the start page")
+    res.render("index");
+});
+router.get("/about", function(req,res){
+
+    console.log("I'm on the start page")
+    res.render("about");
+});
+router.get("/login", function(req,res){
+
+    console.log("I'm on the start page")
+    res.render("about");
+});
+
+module.exports= router;
